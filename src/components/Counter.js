7effb59bcfdf477f99c93f5263ada1d8bdfc3a27
GitHubKrishnaKumar <kksoni1432@gmail.com>
2023-07-03 16:57:17 +0530

    submitting code-Mon Jul 03 2023 16:57:13 GMT+0530 (India Standard Time)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,10 @@ import { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(0);
   return (
-    <>
+    <div>
       <p>Button clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Clike me</button>
-    </>
+    </div>
   );
 };
 
